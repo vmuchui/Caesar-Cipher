8f9a9b24b6b64553570e978dcc906a14f39cba97
vmuchui <kaigongimuchui@gmail.com>
2019-05-15 10:51:13 +0300

    adds the user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,9 @@ public class App{
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a name you would like to be encrypted!");
         String inputtedName = myConsole.readLine();
+        System.out.println("Enter a number you would like to use as the key for the encryption formula:");
+        int inputtedKey = scanner.nextInt();
+        String encryptedWord = CaesarCipher.encryptName(inputtedName,inputtedKey);
+        System.out.println("Your encryption for " + inputtedName + "is: " + encryptedWord);
     }
-}mYea
+}
