21ab77633f4728eaedb0cdd4578b64e5a58845b5
vmuchui <kaigongimuchui@gmail.com>
2019-05-15 11:37:20 +0300

    added the decryption logic.

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -20,4 +20,23 @@ public class CaesarCipher {
 
         return result;
     }
+    public static String decryptName(String name, int key) {
+        if (key > 26) {
+            key = key%26;
+        }
+        else if(key<0) {
+            key = (key%26) + 26;
+        } 
+        String result = "";
+        int length = name.length();
+        for (int i = 0; i <=length-1; i++) {
+            char character = name.charAt(i);
+            char ch = (char) (character - key);
+            result += ch;
+        }
+
+
+
+        return result;
+    }
 }
