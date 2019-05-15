72a3468041c5c8ecbb13b9ba260ebddab783398f
vmuchui <kaigongimuchui@gmail.com>
2019-05-15 22:50:53 +0300

    added tests to the file.

--- src/test/CaesarCipherTest.java
+++ src/test/CaesarCipherTest.java
@@ -5,15 +5,26 @@ import static org.junit.Assert.*;
 
 public class CaesarCipherTest {
 
+    @Test
+    public void encryptName_takesArguments_true() {
+       
+        CaesarCipher newOne = new CaesarCipher();
+        assertEquals(true, newOne.encryptName(word, 12));
+    }
+
 
     @Test
-    public void encryptName_takesStringsAsArguments_String () {
-        CaesarCipher expected = new CaesarCipher();
-        assertEquals(weare,expected.encryptName(weare));
+    public void encryptName_takesStringsAsArguments_wear () {        
+        assertEquals("wear",CaesarCipher.encryptName(wear));
     }
     @Test
-    public void encryptName_returnsAnEncryptedString_String(){
-        CaesarCipher expectedResult = new CaesarCipher();
-        assertEquals(y,expectedResult.encryptName(1));
+    public void encryptName_takesKeyAsAnInteger_2(){
+        String testWord = "a";
+        int testNumber = 1;
+        assertEquals("b",CaesarCipher.encryptName(testWord,testNumber));
+    }
+    @Test
+    public void encryptName_returnsString_b() {
+        assertEquals("b", CaesarCipher.encryptName("a", 1));
     }
 }
