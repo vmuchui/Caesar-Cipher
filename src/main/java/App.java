7e1bdb6fb1b94437e6864bdd2708271984db2ea7
vmuchui <kaigongimuchui@gmail.com>
2019-05-21 11:31:17 +0300

    refactored the user interface.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,33 +1,19 @@
 import java.util.Scanner;
-import java.io.Console;
 
 
 public class App{
     public static void main(String[] args) {
-        Console myConsole = System.console();
-        Scanner scanner = new Scanner(System.in);
-        boolean runProgram = true;
-        while (runProgram=true) {
-            System.out.println("Would you like to: 1.Encrypt or 2.Decrypt 3.Exit?");
-            String inputtedChoice = myConsole.readLine();
-            if (inputtedChoice.equals("Encrypt")) {
-                System.out.println("Enter the word you would like to encrypt: ");
-                String inputtedName = myConsole.readLine();
-                System.out.println("Enter the key you would like to use for your encryption!");
-                int inputtedKey = scanner.nextInt();
-                String encryptedWord = CaesarCipher.encryptName(inputtedName,inputtedKey);
-                System.out.println("Your encryption for " + inputtedName + " is: " + encryptedWord);
-            }else if (inputtedChoice.equals("Decrypt")) {
-                System.out.println("Enter the word you would like to decrypt: ");
-                String inputtedName = myConsole.readLine();
-                System.out.println("Enter the key for your decryption!");
-                int inputtedKey = scanner.nextInt();
-                String encryptedWord = CaesarCipher.decryptName(inputtedName,inputtedKey);
-                System.out.println("Your decryption for " + inputtedName + " is: " + encryptedWord);
-            }else {
-                runProgram = false;
-            }
-        }
-       
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter the word you would like to encrypt and decrypt: ");
+        String inputtedName = myScanner.nextLine();    
+        System.out.println("Enter the key you would like to use for your encryption!");        
+        int inputtedKey = myScanner.nextInt();       
+        CaesarCipher createdCaesarCipher = new CaesarCipher(inputtedName, inputtedKey);          
+        String encrypted = CaesarCipher.encrypt(createdCaesarCipher);
+        CaesarCipher decrypt = new CaesarCipher(encrypted, inputtedKey);
+        String decrypted = CaesarCipher.decrypt(decrypt);
+        System.out.println("Encryption: " + encrypted);
+        System.out.println("Decryption: " + decrypted);
     }
 }
