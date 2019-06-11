08016466d34c52a03c4e6fe55ef63cc943c027f6
vmuchui <kaigongimuchui@gmail.com>
2019-06-11 14:54:47 +0300

    added test to check for capital letters.

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -8,12 +8,12 @@ public class CaesarCipherTest {
     @Test
     public void CaesarCipher_instantiatesCorrectly_true() {
         CaesarCipher newOne = new CaesarCipher("name", 1);
-        assertEquals(true, newOne instanceof CaesarCipher);
+        assertTrue(newOne instanceof CaesarCipher);
     }
 
 
     @Test
-    public void CaesarCipher_takesStringsAsArguments_String () { 
+    public void CaesarCipher_takesStringsAsArguments_name () { 
         CaesarCipher newOne = new CaesarCipher("name", 12);      
         assertEquals("name",newOne.getName());
     }
@@ -23,23 +23,43 @@ public class CaesarCipherTest {
         assertEquals(12, foo.getKey());
     }
     @Test
-    public void encrypt_takesCaesarCipherAsArgument_String() {
+    public void encrypt_returnsString_String() {
         CaesarCipher foo = new CaesarCipher("name", 1);
-        assertEquals("obnf", CaesarCipher.encrypt(foo));
+        assertEquals("obnf", foo.encrypt());
     }
     @Test
-    public void decrypt_takesCaesarCipherAsArgument_String() {
+    public void decrypt_returnsString_String() {
         CaesarCipher foo = new CaesarCipher("obnf", 1);
-        assertEquals("name", CaesarCipher.decrypt(foo));
+        assertEquals("name", foo.decrypt());
     }
     @Test
-    public void decrypt_doesNotTakeKeyWithValueOfMoreThan26_name() {
-        CaesarCipher foo = new CaesarCipher("name", 234);
-        assertEquals("name", CaesarCipher.decrypt(foo));
+    public void encrypt_doesNotReturnNonLetterCharacters_b() {
+        CaesarCipher foo = new CaesarCipher("z", 1);
+        assertEquals("a", foo.encrypt());
     }
     @Test
-    public void encrypt_doesNotTakeKeyWithValueOfMoreThan26_name() {
-        CaesarCipher foo = new CaesarCipher("name", 234);
-        assertEquals("name", CaesarCipher.encrypt(foo));
+    public void decrypt_doesNotReturnNonLetterCharacters_y() {
+        CaesarCipher foo = new CaesarCipher("a", 1);
+        assertEquals("z", foo.decrypt());
+    }
+    @Test
+    public void encrypt_doesNotTakeKeyValueLessThan0_name() {
+        CaesarCipher foo = new CaesarCipher("a", -1);
+        assertEquals("a", foo.encrypt()); 
+    }
+    @Test
+    public void decrypt_doesNotTakeKeyValuesLessThan0_name() {
+        CaesarCipher foo = new CaesarCipher("name", -2);
+        assertEquals("name", foo.decrypt()); 
+    }
+    @Test
+    public void encrypt_returnsLettersForCapitalletters_String() {
+        CaesarCipher foo = new CaesarCipher("ZIC", 2);
+        assertEquals("BKE", foo.encrypt());
+    }
+    @Test
+    public void decrypt_returnsLettersForCapitalletters_String() {
+        CaesarCipher foo = new CaesarCipher("A", 1);
+        assertEquals("z", foo.decrypt());
     }
 }
